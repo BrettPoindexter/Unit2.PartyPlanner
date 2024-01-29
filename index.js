7b220455a7ed06e26c79c336103aba9caddba581
Brett Poindexter <poindexterbrett@gmail.com>
2024-01-28 21:23:10 -0500

    Making progress, cannot access state.events

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const cohort = '2311-FSA-ET-WEB-PT-SF';
 const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${cohort}/events`;
 const eventElement = document.querySelector('#events');
-const deleteButton = document.querySelector('.delete');
 
-
-const state = {
-    events: [],
-};
+const state = {events: []};
+console.log(state);
+console.log(state.events);
 
 async function render() {
     await getEvent();
@@ -14,13 +12,7 @@ async function render() {
 };
 
 render();
-console.log(state);
 
-document.addEventListener('click', function (event) {
-    if (event.target.classList.contains('delete')) {
-        deleteEvent();
-    }
-});
 
 async function getEvent() {
     try {
@@ -33,7 +25,7 @@ async function getEvent() {
 };
 
 function renderEvent() {
-    if(!state.events.length) {
+    if(state.events.length === 0) {
         eventElement.innerHTML = '<li>No Events</li>';
         return;
     }
@@ -45,11 +37,13 @@ function renderEvent() {
         <h3 class='date'>Date: ${i.date}</h3>
         <h3 class='location'>Where: ${i.location}</h3>
         <p class='description'>${i.description}</p>
-        <button class='delete' onClick='${deleteEvent}'>Delete</button>
+        <button class='delete' id='${i.id}'>Delete</button>
         `;
+        const deleteButton = li.querySelector('.delete');
+        deleteButton.addEventListener('click', deleteEvent);
         return li;
     });
-    
+
     eventElement.replaceChildren(...eventPost);
 }
 
@@ -61,10 +55,11 @@ async function addEvent(event) {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
-                name: eventElement.name.value,
-                date: eventElement.date.value,
-                description: eventElement.description.value,
-                location: eventElement.location.value,
+                id: event.target.id.value,
+                name: event.target.name.value,
+                description: event.target.description.value,
+                date: event.target.date.value,
+                location: event.target.location.value,
             }),
         });
         
@@ -78,13 +73,9 @@ async function addEvent(event) {
     }
 };
 
-document.addEventListener('click', function(event) {
-    if (event.target.classList.contains('delete')) {
-        deleteEvent(event);
-    }
-});
+function deleteEvent(event) {
+    event.preventDefault();
 
-function deleteEvent(i) {
-    state.events.pop(i);
-    renderEvent();
+    const target = event.target.id;
+    console.log(target);
 }
